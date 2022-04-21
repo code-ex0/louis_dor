cd1b0785c00c4d797a1bc24d78c0a50acc6dacd9
code-ex0 <louis.sasse@protonmail.com>
2022-04-21 09:02:24 +0200

    feat(blockchain): wallet
    add methode for convert keypair

--- src/blockchain/wallet.rs
+++ src/blockchain/wallet.rs
@@ -2,6 +2,7 @@ use ed25519_dalek::Keypair;
 use ed25519_dalek::PublicKey;
 use ed25519_dalek::SecretKey;
 use rand::rngs::OsRng;
+use rustc_serialize::hex::{FromHex, ToHex};
 
 #[derive(Debug)]
 pub struct Wallet {
@@ -11,11 +12,43 @@ pub struct Wallet {
 
 impl Wallet {
     pub fn new() -> Self {
-        let mut csprng = OsRng {};
-        let keypair: Keypair = Keypair::generate(&mut csprng);
+        let mut rng = OsRng {};
+        let keypair: Keypair = Keypair::generate(&mut rng);
         Wallet {
             public_key: keypair.public,
             secret_key: keypair.secret,
         }
+    }
+
+    pub fn from_string(secret_key: String, public_key: String) -> Keypair {
+        Keypair::from_bytes(&[secret_key.from_hex().unwrap(), public_key.from_hex().unwrap()].concat()).expect("Failed to create keypair")
+    }
+
+    pub fn from_public_string(public_key: &String) -> PublicKey {
+        PublicKey::from_bytes(&public_key.from_hex().unwrap()).expect("Failed to create public key")
+    }
+
+    pub fn from_secret_string(secret_key: String) -> SecretKey {
+        SecretKey::from_bytes(&secret_key.from_hex().unwrap()).expect("Failed to create secret key")
+    }
+
+    pub fn from_secret_key(secret_key: SecretKey) -> Self {
+        let public_key = PublicKey::from(&secret_key);
+        Wallet {
+            public_key,
+            secret_key,
+        }
+    }
+
+    pub fn to_keypair(&self) -> Keypair {
+        Keypair::from_bytes(&[self.secret_key.to_bytes(), self.public_key.to_bytes()].concat()).expect("Failed to create keypair")
+    }
+
+    pub fn public_to_string(&self) -> String {
+        self.public_key.to_bytes().to_hex()
+    }
+
+    pub fn secret_to_string(&self) -> String {
+        self.secret_key.to_bytes().to_hex()
     }
 }
