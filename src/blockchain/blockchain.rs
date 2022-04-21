dd2be7ae7ef4547e885849065c90cd87861035a6
code-ex0 <louis.sasse@protonmail.com>
2022-04-21 08:47:04 +0200

    fix(blockchain): check transaction
    add temps transaction for the verification

--- src/blockchain/blockchain.rs
+++ src/blockchain/blockchain.rs
@@ -4,7 +4,7 @@ use crate::current_time;
 #[derive(Debug, Clone)]
 pub struct Blockchain {
     pub chain: Vec<Block>,
-    pub temp_transactions: Vec<Transaction>,
+    pub temp_transactions: Vec<Transaction>
 }
 
 impl Blockchain {
@@ -21,6 +21,14 @@ impl Blockchain {
     }
 
     pub fn new_transaction(&mut self, transaction: Transaction) {
+        if let Some(sender) = &transaction.sender {
+            let sender_balance = self.calculate_balance(sender.clone());
+            let sender_temp_balance = self.calculate_temps_balance(sender.clone());
+            if sender_balance < transaction.amount + sender_temp_balance {
+                println!("Sender does not have enough balance to send transaction");
+                return;
+            }
+        }
         if transaction.verify() {
             self.temp_transactions.push(transaction);
         }
@@ -30,8 +38,9 @@ impl Blockchain {
         self.chain.last().unwrap()
     }
 
-    pub fn mine_block(&mut self, difficulty: u64) {
+    pub fn mine_block(&mut self, difficulty: u64, address: String, reward: u64) {
         let mut block = Block::new(
+            self.last_block().index + 1,
             self.last_block().hash.clone(),
             current_time(),
             self.temp_transactions.clone(),
@@ -40,5 +49,49 @@ impl Blockchain {
         block.mine_block(difficulty);
         self.chain.push(block);
         self.temp_transactions = Vec::new();
+    }
+
+    pub fn calculate_balance(&self, address: String) -> u64 {
+        let mut balance = 0;
+        for block in self.chain.iter() {
+            for transaction in block.transactions.iter() {
+                if transaction.sender == Some(address.clone()) {
+                    balance -= transaction.amount;
+                }
+                if transaction.receiver == Some(address.clone()) {
+                    balance += transaction.amount;
+                }
+            }
+        }
+        balance
+    }
+
+    pub fn calculate_temps_balance(&self, address: String) -> u64 {
+        let mut balance = 0;
+        for transaction in self.temp_transactions.iter() {
+            if transaction.sender == Some(address.clone()) {
+                balance += transaction.amount;
+            }
+        }
+        balance
+    }
+
+    pub fn get_wallets(&self) -> Vec<String> {
+        let mut wallets = Vec::new();
+        for block in self.chain.iter() {
+            for transaction in block.transactions.iter() {
+                if let Some(sender) = transaction.sender.clone() {
+                    if !wallets.contains(&sender) {
+                        wallets.push(sender);
+                    }
+                }
+                if let Some(receiver) = transaction.receiver.clone() {
+                    if !wallets.contains(&receiver) {
+                        wallets.push(receiver);
+                    }
+                }
+            }
+        }
+        wallets
     }
 }
