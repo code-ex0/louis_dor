ec85b65689a6c1b35d70059bc6a351bc3ff47662
code-ex0 <louis.sasse@protonmail.com>
2022-04-21 09:01:06 +0200

    refactor(blockchain): transaction
    reformate all transaction file

--- src/blockchain/transaction.rs
+++ src/blockchain/transaction.rs
@@ -1,22 +1,35 @@
 use louis_dor::current_time;
-use ed25519_dalek::{Keypair, Signer, Verifier};
+use ed25519_dalek::{Keypair, SignatureError, Signer, Verifier};
 use ed25519_dalek::PublicKey;
 use ed25519_dalek::Signature;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use rustc_serialize::hex::{ToHex, FromHex};
+use crate::Wallet;
 
 #[derive(Debug, Clone)]
 pub struct Transaction {
-    pub sender: Option<PublicKey>,
-    pub receiver: Option<PublicKey>,
+    pub sender: Option<String>,
+    pub receiver: Option<String>,
     pub amount: u64,
     pub timestamp: u64,
-    pub signature: Option<Signature>,
+    pub signature: Option<String>,
 }
 
 impl Transaction {
 
+    pub fn new(sender: Option<String>, receiver: Option<String>, amount: u64) -> Transaction {
+        Transaction {
+            sender,
+            receiver,
+            amount,
+            timestamp: current_time(),
+            signature: None,
+        }
+    }
+
     pub fn bytes (&self) -> Vec<u8> {
+
         let mut bytes = vec![];
         if let Some(sender) = &self.sender {
             bytes.extend_from_slice(sender.as_bytes());
@@ -35,20 +48,34 @@ impl Transaction {
         hasher.result_str().into_bytes()
     }
 
+    pub fn new_genesis_transaction() -> Transaction {
+        // let mut wallet = Wallet::new();
+        // println!("Secret key : {}", wallet.secret_to_string());
+        // println!("Public Key : {}", wallet.public_to_string());
+        let mut transaction = Transaction::new(None, Some("f30bcd6738f87172ec16e23e4bb8944275e568b04e118732099b4185b925e01a".to_string()), 21_000_000_000_000_000);
+        transaction.signature = None;
+        transaction
+    }
+
     pub fn sign(&mut self, key: Keypair) {
-        if let Some(p) = self.sender {
-            if p != key.public {
-                panic!("you can't sign for a other account")
-            } else {
-                self.signature = Some(key.sign(&self.hash()));
-            }
+        if self.sender == self.receiver {
+            println!("You can't send money to yourself");
+            return;
         }
+        self.signature = Some(key.sign(&self.hash()).to_bytes().to_hex());
     }
 
     pub fn verify(&self) -> bool {
-        match (self.sender, self.signature) {
-            (Some(p), Some(s)) if p.verify(&self.hash(), &s).is_ok() => true,
-            (None, _) => true, // for miner
+        match (&self.signature, &self.sender) {
+            (Some(signature), Some(sender)) => {
+                let public_key = PublicKey::from_bytes(&sender.from_hex().unwrap()).unwrap();
+                let signature = Signature::from_bytes(&signature.from_hex().unwrap()).unwrap();
+                public_key.verify(&self.hash(), &signature).is_ok()
+            },
+            (None, _) => {
+                println!("You have to sign the transaction before verifying it");
+                true
+            },
             _ => false
         }
     }
